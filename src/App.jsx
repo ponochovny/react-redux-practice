12699c73b63044c8606cba7407d4fb87b15848f9
ponochovny <ponochovny@gmail.com>
2022-07-14 07:10:07 +0300

    added redux store and button to change the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,77 @@
 import './App.scss'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
 
 function App() {
+	const dispatch = useDispatch()
+	const persons = useSelector((state) => state.persons)
+
+	const randomIntFromInterval = (min, max) => {
+		// min and max included
+		return Math.floor(Math.random() * (max - min + 1) + min)
+	}
+
+	useEffect(() => {
+		console.log(persons)
+	}, [])
+
 	return (
 		<div className='App'>
-			<div className="App__title">
-				<h1>App to practice Redux!</h1><small>and thunk</small>
+			<div className='App__title'>
+				<h1>🥳App to practice Redux!</h1>
+				<small>and thunk</small>😏
 			</div>
-			<div className="App__container">
-				<div className="App__sidebar left">
-					<div className="block">
-						<div className="block__title">Controls #1</div>
-						<div className="block__content">
-content
+			<div className='App__container'>
+				<div className='App__sidebar left'>
+					<div className='block'>
+						<div className='block__title'>Controls #1</div>
+						<div className='block__content'>content</div>
+					</div>
+				</div>
+				<div className='App__content'>
+					<div className='content'>
+						<div className='content__title'>Here is Store data. Change it!</div>
+						<div className='content__data'>
+							<div className='content__list'>
+								{persons.map((el) => (
+									<div className='person' key={el.id}>
+										<div
+											className='person__img'
+											style={{
+												backgroundImage: `url(${'https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=766&q=80'})`,
+											}}
+										></div>
+										<div className='person__content'>
+											<div className='person__name'>{el.name}</div>
+											<div className='person__description'>
+												{el.description}
+											</div>
+											<div className='person__salary'>${el.salary}</div>
+										</div>
+									</div>
+								))}
+							</div>
 						</div>
 					</div>
 				</div>
-				<div className="App__content">
-					<div className="content">
-						<div className="content__title">Here is Store data. Change it!</div>
-						<div className="content__data">
-<div className="content__list">
-	<div className="person">
-		<div className="person__img" style={{backgroundImage: `url(${'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'})`}}></div>
-		<div className="person__content">
-			<div className="person__name">Name</div>
-			<div className="person__description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet fugiat ut</div>
-			<div className="person__salary">500$</div>
-		</div>
-	</div>
-</div>
-						</div>
-					</div>
-				</div>
-				<div className="App__sidebar right">
-					<div className="block">
-						<div className="block__title">Controls #2</div>
-						<div className="block__content">
-<button>Add person</button>
+				<div className='App__sidebar right'>
+					<div className='block'>
+						<div className='block__title'>Controls #2</div>
+						<div className='block__content'>
+							<button disabled>Add person</button>
+							<button
+								onClick={() =>
+									dispatch({
+										type: 'CHANGE_SALARY_RANDOMLY',
+										payload: {
+											personId: 1,
+											newSalary: randomIntFromInterval(300, 2500),
+										},
+									})
+								}
+							>
+								Change salary (#1)
+							</button>
 						</div>
 					</div>
 				</div>
--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,50 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.scss'
 import App from './App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+const INITIAL_STATE = {
+	persons: [
+		{
+			id: 1,
+			name: 'Ismail Erenber',
+			description:
+				'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet fugiat ut',
+			salary: 500,
+			picture:
+				'https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=766&q=80',
+		},
+	],
+}
+
+const changePersonSalary = (state, { personId, newSalary }) => {
+	const newPersons = [...state.persons]
+	const index = newPersons.findIndex((el) => el.id === personId)
+	newPersons[index].salary = newSalary
+
+	return {
+		...state,
+		persons: [...newPersons],
+	}
+}
+
+const reducer = (state = INITIAL_STATE, action) => {
+	switch (action.type) {
+		case 'CHANGE_SALARY_RANDOMLY':
+			return changePersonSalary(state, action.payload)
+		default:
+			return state
+	}
+}
+
+let store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>
 )
