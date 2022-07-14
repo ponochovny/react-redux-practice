b259421b47a1e79f41aa894c187c2ae89f6519aa
ponochovny <ponochovny@gmail.com>
2022-07-14 07:20:03 +0300

    added persons, added fixes

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
 										<div
 											className='person__img'
 											style={{
-												backgroundImage: `url(${'https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=766&q=80'})`,
+												backgroundImage: `url(${el.picture})`,
 											}}
 										></div>
 										<div className='person__content'>
--- src/index.jsx
+++ src/index.jsx
@@ -16,6 +16,33 @@ const INITIAL_STATE = {
 			picture:
 				'https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=766&q=80',
 		},
+		{
+			id: 2,
+			name: 'Bella Berger',
+			description:
+				'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet fugiat ut',
+			salary: 750,
+			picture:
+				'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+		},
+		{
+			id: 3,
+			name: 'Arina Solnair',
+			description:
+				'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet fugiat ut',
+			salary: 700,
+			picture:
+				'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+		},
+		{
+			id: 4,
+			name: 'Bill Norbery',
+			description:
+				'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet fugiat ut',
+			salary: 1500,
+			picture:
+				'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+		},
 	],
 }
 
